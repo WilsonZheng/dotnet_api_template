5c8bd799c858cb62aa788e7b135cee7ff0210b99
Wilson Zheng <wilson.zheng@vacasa.com>
2024-05-15 23:33:52 +1200

    init commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApiProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+             "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var range = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = range.Next(-20, 55),
+                Summary = Summaries[range.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Kata
+
+Environment set up: Download .Net SDK 8
+
+# Command to set up project:
+
+`dotnet new webapi -n kataApi`
+
+# Start API server:
+
+`dotnet run`
